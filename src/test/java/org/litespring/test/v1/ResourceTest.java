32e5452eae3073e7f2f4281e2aff55bc0e83d324
haoyoulove <584703362@qq.com>
2018-07-01 19:02:06 +0800

    修改路径

--- src/test/java/org/litespring/test/v1/ResourceTest.java
+++ src/test/java/org/litespring/test/v1/ResourceTest.java
@@ -36,7 +36,7 @@ public class ResourceTest {
 	@Test
 	public void testFileSystemResource() throws Exception {
 
-		Resource r = new FileSystemResource("E:\\springworkspace\\litespring\\src\\test\\resource\\petstore-v1.xml");
+		Resource r = new FileSystemResource("src\\test\\resource\\petstore-v1.xml");
 
 		InputStream is = null;
 		try {
